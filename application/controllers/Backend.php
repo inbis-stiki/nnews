d372c5ddc3b8f35015cbb9f8f4ed243db9055918
devisap <ilham.sagitaputra@gmail.com>
2020-12-13 19:39:39 +0700

    setup

--- application/controllers/Backend.php
+++ application/controllers/Backend.php
@@ -2,7 +2,7 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Backend extends CI_Controller {
-
+// sgdsfg
 	function __construct(){
 		parent::__construct();
 		$this->load->model('Mbackend');
