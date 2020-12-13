6f339a31b380085350b2553c5025b347be904ed5
devisap <ilham.sagitaputra@gmail.com>
2020-12-13 19:32:15 +0700

    set up config

--- application/config/database.php
+++ application/config/database.php
@@ -75,7 +75,7 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'localhost',
+	'hostname' => 'kristomoyo.com',
 	'username' => 'panji',
 	'password' => '1sampai8',
 	'database' => 'postgres',
