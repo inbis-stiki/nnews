b067f48d6a08fea4bd4d5e72f2c3e2694dc81423
Ilham Sagita Putra <ilham.sagitaputra@gmail.com>
2021-04-15 23:14:15 +0700

    styling login  2

--- application/views/login.php
+++ application/views/login.php
@@ -14,7 +14,7 @@
         <div class="container">
             <div class="row">
                 <div class="col-md-12">
-                    <img class="d-block img-fluid mx-auto" src="<?= base_url('assets/image/logo.png') ?>" width="150" style="margin-bottom: 25px;">
+                    <img class="d-block img-fluid mx-auto" src="<?= base_url('assets/image/logo.png') ?>" width="150" style="margin-top: 25px; margin-bottom: 25px;">
                 </div>
             </div>
             <div class="row">
